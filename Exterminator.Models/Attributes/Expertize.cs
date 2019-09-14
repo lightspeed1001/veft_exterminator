23ae45da4dfbb95962d9a9add7b40fe01ed36384
eytor <eythoras16@ru.is>
2019-09-14 18:15:01 +0000

    custom exception

--- Exterminator.Models/Attributes/Expertize.cs
+++ Exterminator.Models/Attributes/Expertize.cs
@@ -1,13 +1,39 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Exterminator.Models.InputModels;
 
 namespace Exterminator.Models.Attributes
 {
     //TODO ?????????
-    public class Expertize : Attribute
+    public class Expertize : ValidationAttribute
     {
-        public Expertize()
+        private string[] _expertize;
+
+        public Expertize(string[] expertize)
         {
+            _expertize = expertize;
         }
+
+        protected override ValidationResult IsValid(
+            object value, ValidationContext validationContext)
+        {
+            var ghostBuster = (GhostbusterInputModel)validationContext.ObjectInstance;
+
+            if (!_expertize.Contains(ghostBuster.Expertize))
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
+
+
+        public string GetErrorMessage()
+        {
+            var availableExpertize = String.Join(", ", _expertize);
+            return $"Expertize can only be one of the following expertize: {availableExpertize}.";
+        }
+
     }
 }
--- Exterminator.Models/InputModels/GhostbusterInputModel.cs
+++ Exterminator.Models/InputModels/GhostbusterInputModel.cs
@@ -10,7 +10,7 @@ namespace Exterminator.Models.InputModels
         [MaxLength(50)]
         public string Name { get; set; }
         [Required]
-        [Expertize]
+        [Expertize(new string[] {"Ghost catcher", "Ghoul strangler", "Monster encager", "Zombie exploder"})]
         public string Expertize { get; set; }
     }
 }
